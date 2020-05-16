19b22bbf981716e4ac85e6850d6bc758c64b824b
dtinth on Cloud9 <dtinth@spacet.me>
2020-05-16 09:18:17 +0000

    Fix port

--- server/src/main.js
+++ server/src/main.js
@@ -75,6 +75,6 @@ const server = new ApolloServer({
   },
 })
 
-server.listen().then(({ url }) => {
+server.listen({ port: +process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`)
 })
